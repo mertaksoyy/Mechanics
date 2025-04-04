0f3bd71038aa519b4f3b600362634c0fc9b59d68
Mert Aksoy <mrtaksy99@gmail.com>
2025-04-04 14:24:17 +0300

    Mechanic
    Pick up mechanic based code added and animation for pick up

---
+++ Pickupitemmechanic/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public bool playerInRange;
+    public string ItemName;
+
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && playerInRange && SelectionManager.Instance.onTarget)
+        {
+            Debug.Log("Item added to inventory");
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
